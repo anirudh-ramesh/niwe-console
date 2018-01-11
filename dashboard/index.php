f8c338fcf0a008243a2aceff54cacf08c1e7ef67
anirudh-ramesh <ramesh.anirudh95@gmail.com>
2018-01-11 16:11:19 +0530

    Fix dashboard/index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,128 +1,129 @@
 <?php
-	include("../check.php");
+	include ("../check.php");
+	include("config/config.php");
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<title>MITRA</title>
-		<!-- Tell the browser to be responsive to screen width -->
+		<!--<meta http-equiv="refresh" content="240">-->
+		<meta name="description" content="Solar plant operational intelligence software for O&M "><meta name="robots" content="index,follow">
+		<meta name="author" content="Suraj Anand">
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-		<!-- Bootstrap 3.3.6 -->
-		<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-		<!-- Font Awesome -->
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-		<!-- Ionicons -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-		<!-- fullCalendar 2.2.5-->
-		<link rel="stylesheet" href="../../plugins/fullcalendar/fullcalendar.min.css">
-		<link rel="stylesheet" href="../../plugins/fullcalendar/fullcalendar.print.css" media="print">
-		<!-- Theme style -->
-		<link rel="stylesheet" href="../../dist/css/AdminLTE.min.css">
-		<!-- AdminLTE Skins. Choose a skin from the css/skins
-				folder instead of downloading all of them to reduce the load. -->
-		<link rel="stylesheet" href="../../dist/css/skins/_all-skins.min.css">
-		<!-- iCheck -->
-		<link rel="stylesheet" href="../../plugins/iCheck/flat/blue.css">
+		<link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+		<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+		<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<!--added style for input table-->
+		<style>
+			table, th, td {
+				border-collapse: collapse;
+			}
+			th, td {
+				padding: 1px;
+				text-align: left;
+			}
+		</style>
+		<style>
+			select {
+				padding:3px;
+				border-radius:5px;
+				background: #f8f8f8;
+				color:#000;
+
+				outline:none;
+				display: inline-block;
+				width:140px;
+				cursor:pointer;
+				text-align:center;
+				font:inherit;
+			}
+		</style>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	</head>
-	<body class="hold-transition skin-blue sidebar-mini" oncontextmenu="return false">
+	<body class="hold-transition skin-blue sidebar-mini" oncontextmenu="return true" onload="startTime()">
 		<div class="wrapper">
 			<header class="main-header">
-				<!-- Logo -->
-				<a href="../../index.php" class="logo">
-					<!-- mini logo for sidebar mini 50x50 pixels -->
-					<span class="logo-mini"style="font-size:12px;">NIWE Advance Station Console</span>
-					<!-- logo for regular state and mobile devices -->
-					<span class="logo-lg"style="font-size:12px;">NIWE Advance Station Console</span>
+				<a href="index.php" class="logo">
+					<span class="logo-mini" style="font-size:12px;">NIWE Advance Station Console</span>
+					<span class="logo-lg" style="font-size:12px;">NIWE Advance Station Console</span>
 				</a>
-				<!-- Header Navbar: style can be found in header.less -->
 				<nav class="navbar navbar-static-top">
-						<!-- Sidebar toggle button-->
 					<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-					<span class="sr-only">Toggle navigation</span>
+						<span class="sr-only">Toggle navigation</span>
 					</a>
-					<!-- Navbar Right Menu -->
 					<div class="navbar-custom-menu">
 						<ul class="nav navbar-nav">
-							<!-- User Account: style can be found in dropdown.less -->
 							<li class="dropdown user user-menu">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-									<img src="../../dist/img/logo.jpg" class="user-image" alt="User Image">
-									<span class="hidden-xs">
-										<!--<?php echo $login_user;?>-->SGS Weather
-									</span>
+									<img src="dist/img/logo.jpg" class="user-image" alt="User Image">
+									<!-- <?php echo $login_user; ?> -->SGS Weather
 								</a>
 								<ul class="dropdown-menu">
-									<!-- User image -->
 									<li class="user-header">
-										<img src="../../dist/img/logo.jpg" class="img-circle" alt="User Image">
-								</div>
-								<div class="pull-left info">
-										<p>
-												<!--<?php echo $login_user;?>-->SGS Weather
-										</p>
+										<img src="dist/img/logo.jpg" class="img-circle" alt="User Image">
+										<!-- <?php echo $login_user; ?>-->SGS Weather
 									</li>
-									<!-- Menu Body -->
-									<!-- Menu Footer-->
 									<li class="user-footer">
 										<div class="pull-right">
-												<a href="../../../logout.php" class="btn btn-default btn-flat">Sign out</a>
+											<a href="../logout.php" class="btn btn-default btn-flat">Sign out</a>
 										</div>
 									</li>
 								</ul>
 							</li>
-							<!-- Control Sidebar Toggle Button -->
 						</ul>
 					</div>
 				</nav>
 			</header>
-				<!-- Left side column. contains the logo and sidebar -->
 			<aside class="main-sidebar">
 				<section class="sidebar">
 					<div class="user-panel">
 						<div class="pull-left image">
-							<img src="../../dist/img/logo.jpg" class="img-circle" alt="User Image">
+							<img src="dist/img/logo.jpg" class="img-circle" alt="User Image">
 						</div>
 						<div class="pull-left info">
-							<p>
-								<!-- <?php echo $login_user; ?>-->
-								SGS Weather
-							</p>
+							<!-- <?php echo $login_user; ?>-->SGS Weather
 						</div>
 					</div>
 					<ul class="sidebar-menu">
 						<li class="treeview">
-							<a href="../../index.php">
-   							<i class="fa fa-dashboard"></i>
-   							<span>Dashboard</span>
+							<a href="#">
+								<i class="fa fa-dashboard"></i>
+								<span>Dashboard</span>
 							</a>
 							<ul class="treeview-menu">
-								<li><a href="../data_visualization/irradiance_time.php"><i class="fa fa-line-chart"></i>View Irradiance-Time Plot</a></li>
+								<li>
+									<a href="pages/data_visualization/irradiance_time.php">
+										<i class="fa fa-line-chart"></i>View Irradiance-Time Plot
+									</a>
+								</li>
 							</ul>
 						</li>
 						<li>
-							<a href="../data_access/">
-   							<i class="fa fa-download"></i>
-                        <span>Data Access</span>
+							<a href="pages/data_access/">
+								<i class="fa fa-download"></i>
+								<span>Data Access</span>
 							</a>
 						</li>
 						<li>
-							<a href="index.php">
+							<a href="pages/date_configuration/">
 								<i class="fa fa-file-text-o"></i>
-                        <span>Data Config</span>
+								<span>Data Config</span>
 							</a>
 						</li>
 						<li>
-                     <a href="../about/about.php">
-                        <i class="fa fa-info-circle"></i>
-                        <span>About</span>
-                     </a>
-                  </li>
+							<a href="pages/about/about.php">
+								<i class="fa fa-info-circle"></i>
+								<span>About</span>
+							</a>
+						</li>
 					</ul>
 				</section>
 			</aside>
-			<!-- Content Wrapper. Contains page content -->
 			<div class="content-wrapper">
 				<!-- Content Header (Page header) -->
 				<!-- Main content -->
@@ -146,33 +147,34 @@
 				</section>
 				<!-- /.content -->
 			</div>
-				<!-- /.content-wrapper -->
-				<footer class="main-footer">
-					<div class="pull-right hidden-xs">
-							<b>Version</b> 1.0
-					</div>
-					<strong>Copyright &copy; 2017 <a href="http://soreva.co.in" target="_blank">Soreva.co.in</a>.</strong> All rights
-					reserved.
-				</footer>
-				<!-- Control Sidebar -->
-				<!-- /.control-sidebar -->
-				<!-- Add the sidebar's background. This div must be placed
-					immediately after the control sidebar -->
+			<footer class="main-footer">
+				<div class="pull-right hidden-xs">
+					<b>Version</b> 1.0
+				</div>
+				<strong>Copyright &copy; 2018 <a href="http://soreva.co.in" target="_blank">Soreva</a></strong> All rights reserved.
+			</footer>
 		</div>
 		<!-- ./wrapper -->
 		<!-- jQuery 2.2.3 -->
-		<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
+		<script src="plugins/jQuery/jquery-2.2.3.min.js"></script>
 		<!-- Bootstrap 3.3.6 -->
-		<script src="../../bootstrap/js/bootstrap.min.js"></script>
-		<!-- Slimscroll -->
-		<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
 		<!-- FastClick -->
-		<script src="../../plugins/fastclick/fastclick.js"></script>
+		<script src="plugins/fastclick/fastclick.js"></script>
 		<!-- AdminLTE App -->
-		<script src="../../dist/js/app.min.js"></script>
-		<!-- iCheck -->
-		<script src="../../plugins/iCheck/icheck.min.js"></script>
-		<!-- Page Script -->
-		<script src="../../dist/js/demo.js"></script>
+		<script src="dist/js/app.min.js"></script>
+		<!-- Sparkline -->
+		<script src="plugins/sparkline/jquery.sparkline.min.js"></script>
+		<!-- jvectormap -->
+		<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+		<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+		<!-- SlimScroll 1.3.0 -->
+		<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+		<!-- ChartJS 1.0.1 -->
+		<script src="plugins/chartjs/Chart.min.js"></script>
+		<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+		<script src="dist/js/pages/dashboard2.js"></script>
+		<!-- AdminLTE for demo purposes -->
+		<script src="dist/js/demo.js"></script>
 	</body>
 </html>
