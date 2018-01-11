5b698102694e831a20ef3db3ac95e9e4e5aa0bcb
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2018-01-11 09:00:56 +0530

    Fix misaligned profile tile

--- dashboard/pages/about/about.php
+++ dashboard/pages/about/about.php
@@ -45,34 +45,25 @@
 					<!-- Navbar Right Menu -->
 					<div class="navbar-custom-menu">
 						<ul class="nav navbar-nav">
-							<!-- User Account: style can be found in dropdown.less -->
 							<li class="dropdown user user-menu">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 									<img src="../../dist/img/logo.jpg" class="user-image" alt="User Image">
-									<span class="hidden-xs">
-										<!--<?php echo $login_user;?>-->SGS Weather
-									</span>
+									<!-- <?php echo $login_user; ?>-->SGS Weather
 								</a>
 								<ul class="dropdown-menu">
-									<!-- User image -->
 									<li class="user-header">
 										<img src="../../dist/img/logo.jpg" class="img-circle" alt="User Image">
-								</div>
-								<div class="pull-left info">
 										<p>
-												<!--<?php echo $login_user;?>-->SGS Weather
+											<!-- <?php echo $login_user; ?>-->SGS Weather
 										</p>
 									</li>
-									<!-- Menu Body -->
-									<!-- Menu Footer-->
 									<li class="user-footer">
 										<div class="pull-right">
-												<a href="../../../logout.php" class="btn btn-default btn-flat">Sign out</a>
+											<a href="../../../logout.php" class="btn btn-default btn-flat">Sign out</a>
 										</div>
 									</li>
 								</ul>
 							</li>
-							<!-- Control Sidebar Toggle Button -->
 						</ul>
 					</div>
 				</nav>
