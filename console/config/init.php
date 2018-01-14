df4b9065f0f8ffd26846cd492fed3074fa51dede
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2018-01-14 14:37:00 +0530

    Refactor channel configuration mechanism
    Signed-off-by: Anirudh Ramesh <ramesh.anirudh95@gmail.com>

--- console/config/init.php
+++ console/config/init.php
@@ -19,11 +19,10 @@ $es_functionNumber = "NumFuncion";
 
 $en_stationNumber = "stationNumber";
 $en_wavelength = "wavelength";
+$en_parameters = "parameters";
+$en_parameterNumber = "parameterNumber"
 $en_channelNumber = "channelNumber";
 $en_voltage2irradiance = "voltage2irradiance";
-
-$iChannels = 16;
-$nChannels = 10;
 
 $start_time = "00:00:00";
 $end_time = "23:59:00";
@@ -32,5 +31,8 @@ $delimiter = ",";
 
 $maindatabaseHandle = sqlsrv_connect($serverName, array("Database" => $maindatabaseName, "UID" => $username, "PWD" => $password));
 $sidedatabaseHandle = sqlsrv_connect($serverName, array("Database" => $sidedatabaseName, "UID" => $username, "PWD" => $password));
+
+$nChannels = sqlsrv_fetch_array(sqlsrv_query($sidedatabaseHandle, "SELECT count([" . $en_channelNumber . "]) FROM [" . $sidedatabaseName . "].[dbo].[" . $en_parameters . "];"));
+$iChannels = sqlsrv_fetch_array(sqlsrv_query($sidedatabaseHandle, "SELECT min([" . $en_parameterNumber . "]) FROM [" . $sidedatabaseName . "].[dbo].[" . $en_parameters . "];"));
 
 ?>
