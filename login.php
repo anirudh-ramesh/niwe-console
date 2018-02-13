ccc5cb263b002c8a357ab55ac884958104da1cd8
Anirudh Ramesh <ramesh.anirudh95@gmail.com>
2018-02-13 22:38:00 +0530

    Beautify login.php

--- login.php
+++ login.php
@@ -1,20 +1,19 @@
 <?php
+
 	session_start();
+
 	include("connection.php");
+
 	$error = "";
-	if (isset($_POST["submit"]))
-	{
+
+	if (isset($_POST["submit"])) {
+
 		if (empty($_POST["username"]) || empty($_POST["password"])) {
 			$error = "Both fields are required.";
 		} else {
 
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-
-			$username = stripslashes($username);
-			$password = stripslashes($password);
-
-			$password = md5($password);
+			$username = stripslashes($_POST['username']);
+			$password = md5(stripslashes($_POST['password']));
 
 			$result = sqlsrv_query($connectionHandle, "SELECT [uid] FROM [Soreva].[dbo].[users] WHERE username='$username' and password='$password'");
 			while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
