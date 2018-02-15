9ce30c17543e5e8e00710f3df6adf73c4fc15d77
anirudh-ramesh <ramesh.anirudh95@gmail.com>
2018-02-15 13:04:30 +0530

    Remove original configuration file

--- console/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$serverName = "(local),1433";
-	$database_name = "[MeteoStation4K]";
-	$connectionInfo = array( "Database"=>"MeteoStation4K", "UID"=>"sa", "PWD"=>"sgs@123");
-	$connectionInfo1 = array( "Database"=>"Soreva", "UID"=>"sa", "PWD"=>"sgs@123");
-	$pg_index = sqlsrv_connect( $serverName, $connectionInfo);
-	$pg_index1 = sqlsrv_connect( $serverName, $connectionInfo1);
-?>
